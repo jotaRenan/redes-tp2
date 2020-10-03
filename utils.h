edacf7bb0164f5027735476b8e9fabf3896d7033
Joao Pedro Rosa <joao.rosa@dtidigital.com.br>
2020-10-03 18:44:28 -0300

    Remove metodos inutilizados

--- utils.h
+++ utils.h
@@ -10,5 +10,4 @@
 
 void logexit(const char *msg);
 int addrparse(const char *addrstr, const char *portstr, struct sockaddr_storage *storage);
-void addrtostr(const struct sockaddr *addr, char *str, size_t strsize);
 int server_sockaddr_init(const char *proto, const char *portstr, struct sockaddr_storage *storage);
